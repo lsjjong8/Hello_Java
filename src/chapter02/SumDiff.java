0868a1101141a6d4f9d43b422a7d0f30db806494
Seung-jong <lsjjong8@naver.com>
2024-02-08 12:05:09 +0900

    1. 정수 합과 차 (문자열과 숫자 연결)

---
+++ src/chapter02/SumDiff.java
@@ -0,0 +1,19 @@
+package chapter02;
+
+public class SumDiff {
+    public static void main(String[] args) {
+        // 계산 결과만 표시, 2개의 정수 값 82와 17의 합과 차를 구해서 표시
+        System.out.println(82 + 17); // 정수형 리터럴
+        System.out.println(82 - 17);
+
+        // 계산식과 결과 표시
+        System.out.print("82 + 17 = ");
+        System.out.println(82 + 17);
+        System.out.print("82 - 17 = ");
+        System.out.println(82 - 17);
+
+        // 계산식과 결과를 println을 2번 써서 표시
+        System.out.println();
+        System.out.println();
+    }
+}
