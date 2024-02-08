91b7838778901c42677fb89c87c341f76362203f
Seung-jong <lsjjong8@naver.com>
2024-02-08 12:45:41 +0900

    3. 문제_세 정수 합과 평균 구하기

---
+++ src/chapter02/SumAvrByThree.java
@@ -0,0 +1,21 @@
+package chapter02;
+
+public class SumAvrByThree {
+    public static void main(String[] args) {
+        int x = 63, y = 18, z = 52;
+
+        // 합계 계산
+        int sum = 0;
+        
+        
+        // x, y, z의 값 각각 한 줄에 출력
+        // 예시) x값은 63 입니다.
+
+        // 합계와 평균 각각 한 줄에 출력
+        // 예시) 합계는 000 입니다.
+        
+    }
+}
+
+// 동일한 계산식을 여러번 작성하면 불필요한 계산 시간이 소요
+// 동일한 계산식을 작성하다 실수로 타이핑을 잘못할 가능성 존재
