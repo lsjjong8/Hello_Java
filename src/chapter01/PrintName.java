66672a08ead163dcb0e2512c59c4376e74af4ec8
Seung-jong <lsjjong8@naver.com>
2024-02-08 11:57:32 +0900

    add PrintName.java

---
+++ src/chapter01/PrintName.java
@@ -0,0 +1,9 @@
+package chapter01;
+
+public class PrintName {
+    public static void main(String[] args) {
+        // 1. 한줄에 한 글자씩 자신의 이름을 표시하는 프로그램을 작성
+
+        // 2. 성과 이름 사이에 한번 더 개행하기, 2가지 방법
+    }
+}
