194418163a1b38afa827e928600a86ddfb6d6dfd
Seung-jong <lsjjong8@naver.com>
2024-02-08 12:51:26 +0900

    2. 정수 합과 평균 (변수 선언과 대입, +나눗셈, +오류발생)

--- src/chapter02/SumAvr.java
+++ src/chapter02/SumAvr.java
@@ -17,7 +17,7 @@ public class SumAvr {
         System.out.println("x값은 " + x + "입니다.");
         System.out.println("y값은 " + y + "입니다.");
         System.out.println("합계는 " + (x+y) + "입니다.");
-        System.out.println("평균은 " + (x+y)/2 + "입니다.");
+        System.out.println("평균은 " + (x+y)/2 + "입니다."); // /(슬래쉬) : 나눗셈 연산자
 
         // 출력 비교
         System.out.println("x");
