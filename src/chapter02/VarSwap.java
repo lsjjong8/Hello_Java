6df119f0ab50b2c2683e900d9c18c54116bf3daf
Seung-jong <lsjjong8@naver.com>
2024-02-08 13:40:00 +0900

    4. 문제_변수 스왑

---
+++ src/chapter02/VarSwap.java
@@ -0,0 +1,17 @@
+package chapter02;
+
+public class VarSwap {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 5;
+        int b = 10;
+
+        // a와 b의 값을 바꿔보기
+        // 힌트 : 물컵
+
+
+        // 출력
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
