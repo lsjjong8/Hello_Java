359326a8e3f1f81dbcd276bc61383ebdc1c69ffc
Seung-jong <lsjjong8@naver.com>
2024-02-08 11:18:45 +0900

    add chapter01

---
+++ src/chapter01/HelloJava.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+// 주석 : 메모용, 코드 아님
+public class HelloJava {
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+
+        // 출력문
+        // System.out.print("나는 자바를 배우는 OOO입니다.");
+        // System.out.print("저는 00살 입니다.");
+    }
+}
+// 이전에 메모장으로 작성 및 컴파일 : javac -encoding utf-8 chapter01.HelloJava.java
