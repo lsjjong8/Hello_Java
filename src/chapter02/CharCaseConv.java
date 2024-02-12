413dd281262beced0cdca716caf9c353c222f574
Seungjong Lee <lsjjong8@naver.com>
2024-02-12 16:01:00 +0900

    9. char형 실습

---
+++ src/chapter02/CharCaseConv.java
@@ -0,0 +1,26 @@
+package chapter02;
+
+public class CharCaseConv {
+    public static void main(String[] args) {
+
+        char ch1 = 'a';
+
+        System.out.println("ch1 출력 : " + ch1);
+        System.out.println("'a' -> int형 형변환 후 출력 : " + (int) ch1); // 97
+        System.out.println("Ascii code 97 -> char형 형변환 : " + (char) 97);
+        System.out.println();
+
+        System.out.println("'A' -> int형 형변환 후 출력 : " + (int) 'A');
+        System.out.println("Ascii code 97 -> char형 형변환 : " + (char) 65);
+        System.out.println();
+
+        char ch2 = (char) (ch1 - 32);
+
+        System.out.println("ch2 출력 : " + ch2);
+        System.out.println("ch2 -> int형 형변환 후 출력 : " + (int) ch2); // 65
+        System.out.println("Ascii code 65 -> char형 형변환 : " + (char)(ch1-32));
+
+        // char은 음수 대입 불가
+        // char ch3 = -1;
+    }
+}
