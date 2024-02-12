cdf0406245e7d4d8deaf71fbd833a60179664e36
Seungjong Lee <lsjjong8@naver.com>
2024-02-12 16:09:42 +0900

    10. String형 실습

---
+++ src/chapter02/Address.java
@@ -0,0 +1,21 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class Address {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("주소 : ");
+
+        String address = sc.next();
+
+        System.out.println("첫 번째 입력하신 주소는 " + address + "입니다.");
+        System.out.println();
+
+        sc.nextLine(); // 버퍼에서 개행 제거용
+        address = sc.nextLine();
+
+        System.out.println("두 번째 입력하신 주소는 " + address + "입니다.");
+    }
+}
