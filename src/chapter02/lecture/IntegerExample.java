1732a475e2aa5eb1ad62bc357a365641e0fe485a
Seung-jong <lsjjong8@naver.com>
2024-02-14 09:05:21 +0900

    3. 데이터 타입 - 정수

---
+++ src/chapter02/lecture/IntegerExample.java
@@ -0,0 +1,21 @@
+package chapter02.lecture;
+
+public class IntegerExample {
+    public static void main(String[] args) {
+        byte a = 10;                        // byte형 변수 a
+        // a = 128;                         // 컴파일 오류
+        System.out.println(a);              // 변수 a 출력
+
+        short b = 50;                       // short형 변수 b
+        // b = -32769;                      // 컴파일 오류
+        System.out.println(b);              // 변수 b 출력
+
+        int c = -10;                        // short형 변수 c
+        // c = 2_147_483_648;               // 컴파일 오류
+        System.out.println(c);              // 변수 c 출력
+
+        long d = -50;                       // short형 변수 d
+        // d = -9_223_372_036_854_775_809;  // 컴파일 오류
+        System.out.println(d);              // 변수 d 출력
+    }
+}
