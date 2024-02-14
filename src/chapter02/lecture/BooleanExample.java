4fb98dda99e33a4591b3235875666b9198fd689a
Seung-jong <lsjjong8@naver.com>
2024-02-14 09:14:10 +0900

    6. 데이터 타입 - 논리형

---
+++ src/chapter02/lecture/BooleanExample.java
@@ -0,0 +1,11 @@
+package chapter02.lecture;
+
+public class BooleanExample {
+
+    public static void main(String[] args) {
+
+        boolean isMarried = true;
+        System.out.println(isMarried);
+
+    }
+}
