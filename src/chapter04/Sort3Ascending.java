e2e7ac34a832b265765eecb1b5a652ce8bdea41d
Seungjong Lee <lsjjong8@naver.com>
2024-02-19 23:04:09 +0900

    16. 문제_세 값을 오름차순으로 정렬 (풀이)

--- src/chapter04/Sort3Ascending.java
+++ src/chapter04/Sort3Ascending.java
@@ -17,6 +17,27 @@ public class Sort3Ascending {
 
         // 변수 a, b, c를 작은 순으로 정렬
 
+        // a가 b보다 크면 a와 b를 교환
+        if(a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // b가 c보다 크면 b와 c를 교환
+        if(b > c) {
+            int temp = b;
+            b = c;
+            c = temp;
+        }
+
+        // a가 b보다 크면 a와 b를 교환
+        if(a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
         System.out.println("a <= b <= c가 되도록 정렬했습니다.");
         System.out.println("변수 a는 " + a + "입니다.");
         System.out.println("변수 b는 " + b + "입니다.");
