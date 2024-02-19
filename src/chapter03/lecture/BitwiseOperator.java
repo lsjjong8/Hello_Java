f79210e3d0b4f296028d3aea70a51477e0e554a6
Seung-jong <lsjjong8@naver.com>
2024-02-19 10:51:40 +0900

    9. 비트 연산자

--- src/chapter03/lecture/BitwiseOperator.java
+++ src/chapter03/lecture/BitwiseOperator.java
@@ -5,7 +5,8 @@ package chapter03.lecture;
 public class BitwiseOperator {
     public static void main(String[] args) {
         //#0. 자바코드로 진법 변환, Integer 클래스의 메소드 활용
-        int data =13;
+        // 출력 메소드는 기본적으로 10진수로 변환해서 보여줌
+        int data = 13;
         System.out.println(Integer.toBinaryString(data)); //1101, 2진수
         System.out.println(Integer.toOctalString(data)); //15, 8진수
         System.out.println(Integer.toHexString(data)); //d, 16진수
@@ -13,39 +14,38 @@ public class BitwiseOperator {
 
         System.out.println(Integer.parseInt("1101", 2)); //13
         System.out.println(Integer.parseInt("15", 8)); //13
-        System.out.println(Integer.parseInt("0D", 16)); //13
+        System.out.println(Integer.parseInt("D", 16)); //13
         System.out.println();
 
-        //#1. 다양한 진법 표현
-        // 출력 메소드는 기본적으로 10진수로 변환해서 보여줌
+        //#1. 다양한 진법 사용
         System.out.println(13); // 10진수, 13
         System.out.println(0b1101); // 2진수, 13
         System.out.println(015); // 8진수, 13
-        System.out.println(0x0D); // 16진수, 13
+        System.out.println(0xD); // 16진수, 13
         System.out.println();
 
         //#2. 비트연산자
         //@AND 비트연산자
         System.out.println(3 & 10); //2
         System.out.println(0b0011 & 0b1010); //0b0010 --> 2
-        System.out.println(0x03 & 0x0A); //2
+        System.out.println(0x3 & 0xA); //2
         System.out.println();
 
         //@OR 비트연산자
         System.out.println(3 | 10); //11
         System.out.println(0b0011 | 0b1010); //0b1011 --> 11
-        System.out.println(0x03 | 0x0A); //11
+        System.out.println(0x3 | 0xA); //11
         System.out.println();
 
         //@XOR 비트연산자
         System.out.println(3 ^ 10); //9
         System.out.println(0b0011 ^ 0b1010); //0b1001 --> 9
-        System.out.println(0x03 ^ 0x0A); //9
+        System.out.println(0x3 ^ 0xA); //9
         System.out.println();
 
         //@NOT 비트연산자
         System.out.println(~3); //-4
         System.out.println(~0b0011); //0b1111...1100 -> -4
-        System.out.println(~0x03); //-4
+        System.out.println(~0x3); //-4
     }
 }
