5e5a7b16d9f12f480cabfb7dcb23e5f1d2c9f632
Seung-jong <lsjjong8@naver.com>
2024-02-19 12:44:45 +0900

    12. 최대값 판별 (조건문)

--- src/chapter04/Max2.java
+++ src/chapter04/Max2.java
@@ -2,7 +2,7 @@ package chapter04;
 
 import java.util.Scanner;
 
-public class Max2A {
+public class Max2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
