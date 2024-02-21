a62f6c3e9b71e5feb7fd7412822432a008be5d1a
Seung-jong <lsjjong8@naver.com>
2024-02-21 10:34:42 +0900

    23. 작은 값부터 큰 값까지 열거 (do-while문)

---
+++ src/chapter04/EnumScope.java
@@ -0,0 +1,27 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class EnumScope {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("정수 A:");
+        int a = sc.nextInt();
+
+        System.out.print("정수 B:");
+        int b = sc.nextInt();
+
+        if(a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        do {
+            System.out.println(a + " ");
+            a = a + 1;
+        } while(a <= b);
+        System.out.println();
+    }
+}
