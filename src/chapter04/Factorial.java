b12c1931624f17af99f1364fc9bf7dfd9f94824a
Seung-jong <lsjjong8@naver.com>
2024-02-21 11:31:38 +0900

    27. 문제_factorial (풀이)

--- src/chapter04/Factorial.java
+++ src/chapter04/Factorial.java
@@ -16,12 +16,12 @@ public class Factorial {
         int i = 1;
         
         // factorial 계산
-        /*while(i <= n) { // Bottom-UP
+        while(i <= n) { // Bottom-UP
             factorial *= i; // factorial에 i 곱하기
             i++; // i 증가
         }
 
-        System.out.println("1부터 " + n + "까지의 곱은 " + factorial + "입니다.");*/
+        System.out.println("1부터 " + n + "까지의 곱은 " + factorial + "입니다.");
         // System.out.println("1부터 " + n + "까지의 곱은 " + recursive(n) + "입니다.");
     }
 
