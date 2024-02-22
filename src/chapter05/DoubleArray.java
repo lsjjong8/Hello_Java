e40ceaa06d5d704bb327f47f66217bcbd2bdf9e6
Seung-jong <lsjjong8@naver.com>
2024-02-22 11:26:09 +0900

    1. DoubleArray (배열)

---
+++ src/chapter05/DoubleArray.java
@@ -0,0 +1,14 @@
+package chapter05;
+
+public class DoubleArray {
+    public static void main(String[] args) {
+        double[] a = new double[5];
+
+        // 모든 요소의 값 표시
+        System.out.println("a[" + 0 + "] = " + a[0]);
+        System.out.println("a[" + 0 + "] = " + a[1]);
+        System.out.println("a[" + 0 + "] = " + a[2]);
+        System.out.println("a[" + 0 + "] = " + a[3]);
+        System.out.println("a[" + 0 + "] = " + a[4]);
+    }
+}
