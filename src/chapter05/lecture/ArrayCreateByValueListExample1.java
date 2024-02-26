c9ceb383efeb0cc3c68a6e32d1b5118782dafdb5
Seung-jong <lsjjong8@naver.com>
2024-02-27 08:58:53 +0900

    2. 값 목록으로 배열 생성 (배열)

---
+++ src/chapter05/lecture/ArrayCreateByValueListExample1.java
@@ -0,0 +1,19 @@
+package chapter05.lecture;
+
+public class ArrayCreateByValueListExample1 {
+    public static void main(String[] args) {
+        int[] scores = {83, 90, 87};
+
+        System.out.println("Scores[0] : " + scores[0]);
+        System.out.println("Scores[0] : " + scores[1]);
+        System.out.println("Scores[0] : " + scores[2]);
+
+        int sum = 0;
+        for(int i=0; i<3; i++) {
+            sum += scores[i];
+        }
+        System.out.println("총합 : " + sum);
+        double avg = (double) sum / 3;
+        System.out.println("평균 : " + avg);
+    }
+}
