92db5d802f95f7d4f7c7d5c8e98399d638f46cd8
Seung-jong <lsjjong8@naver.com>
2024-02-27 08:57:32 +0900

    1. 문자열 비교 (String)

---
+++ src/chapter05/lecture/StringEqualsExample.java
@@ -0,0 +1,31 @@
+package chapter05.lecture;
+
+public class StringEqualsExample {
+    public static void main(String[] args) {
+        String str1 = "이름";
+        String str2 = "이름";
+
+        if(str1 == str2) {
+            System.out.println("str1과 str2는 참조가 같음");
+        } else {
+            System.out.println("str1과 str2는 참조가 다름");
+        }
+
+        if(str1.equals(str2)) {
+            System.out.println("str1과 str2는 문자열이 같음");
+        }
+
+        String str3 = new String("이름");
+        String str4 = new String("이름");
+
+        if(str3 == str4) {
+            System.out.println("str3과 str4는 참조가 같음");
+        } else {
+            System.out.println("str3과 str4는 참조가 다름");
+        }
+
+        if(str3.equals(str4)) {
+            System.out.println("str3과 str4는 문자열이 같음");
+        }
+    }
+}
