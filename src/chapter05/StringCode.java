f0eee1009cc809c45fb1cfccb6be0a184643b299
Seung-jong <lsjjong8@naver.com>
2024-02-27 09:52:36 +0900

    3. 각 문자 코드 표시 (String)

---
+++ src/chapter05/StringCode.java
@@ -0,0 +1,18 @@
+package chapter05;
+
+import java.util.Scanner;
+
+public class StringCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("문자열str:");
+
+        String str = sc.next();
+
+        for(int i=0; i<str.length(); i++) {
+            System.out.printf("s[%d] = %c %4x \n", i, str.charAt(i), (int)str.charAt(i));
+        }
+
+    }
+}
