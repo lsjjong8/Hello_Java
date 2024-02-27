b29fc8ea067275e7735e22006559c0ecf8793641
Seung-jong <lsjjong8@naver.com>
2024-02-27 10:04:02 +0900

    4. 문자열 포함 (String)

---
+++ src/chapter05/SearchString.java
@@ -0,0 +1,27 @@
+package chapter05;
+
+import java.util.Scanner;
+
+public class SearchString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("문자열 s1:");
+        String s1= sc.next();
+        System.out.print("문자열 s2:");
+        String s2= sc.next();
+
+        // s1이 s2에 포함되어 있는가?
+        int idx = s1.indexOf(s2);
+
+        if(idx == -1) {
+            System.out.println("s1 안에 s2가 포함돼 있지 않습니다.");
+        } else {
+            System.out.println(s1);
+            for (int i = 0; i < idx; i++) {
+                System.out.print(" ");
+            }
+            System.out.println(s2);
+        }
+    }
+}
