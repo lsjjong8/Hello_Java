51d06feee270acaa80afa66e123017ba3a730c05
Seung-jong <lsjjong8@naver.com>
2024-02-27 12:24:13 +0900

    15. 로또 (배열)

--- src/chapter05/ArrayRandY.java
+++ src/chapter05/ArrayRandY.java
@@ -2,7 +2,7 @@ package chapter05;
 
 import java.util.Random;
 import java.util.Scanner;
-
+//
 public class ArrayRandY {
     public static void main(String[] args) {
         Random rand = new Random();
