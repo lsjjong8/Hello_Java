54ac8c900ec2556af324fc8ce5c996e53a88ce98
Seung-jong <lsjjong8@naver.com>
2024-02-29 11:35:17 +0900

    1.3 super(매개변수) (상속)

--- src/chapter07/lecture/SportsCar.java
+++ src/chapter07/lecture/SportsCar.java
@@ -1,10 +1,15 @@
 package chapter07.lecture;
 
-class SportsCar extends Car {
+public class SportsCar extends Car {
 
     public boolean isCoupe;
 
     public SportsCar() {
         super(); // 상위 클래스에 기본 생성자가 없으면 오류 발생
+    }
+
+    public SportsCar(boolean isCoupe) {
+        super(isCoupe ? 2 : 4);
+        this.isCoupe = isCoupe;
     }
 }
