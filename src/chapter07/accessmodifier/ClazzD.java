2f6f62860981de4759b6c6f24c7d4dbebf2667d5
Seung-jong <lsjjong8@naver.com>
2024-02-29 12:26:34 +0900

    4. protected (접근 제한자, 오류 주석 처리)

--- src/chapter07/accessmodifier/ClazzD.java
+++ src/chapter07/accessmodifier/ClazzD.java
@@ -4,8 +4,8 @@ import chapter07.lecture.ClazzA;
 
 public class ClazzD {
     public void method() {
-        ClazzA a = new ClazzA(); // 생성자 호출 불가
-        a.field = "value"; // 필드 접근 불가
-        a.method(); // 메서드 접근 불가
+        // ClazzA a = new ClazzA(); // 생성자 호출 불가
+        // a.field = "value"; // 필드 접근 불가
+        // a.method(); // 메서드 접근 불가
     }
 }
