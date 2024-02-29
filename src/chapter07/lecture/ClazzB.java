19ccb67432fbe793e9ac5a9ec1c8247d41a2710e
Seung-jong <lsjjong8@naver.com>
2024-02-29 12:00:57 +0900

    4. protected (접근 제한자)

--- src/chapter07/lecture/ClazzB.java
+++ src/chapter07/lecture/ClazzB.java
@@ -2,7 +2,7 @@ package chapter07.lecture;
 
 public class ClazzB {
     public void method() {
-        ClazzA a = new ClazzA(); // 접근 가능
+        A a = new A(); // 접근 가능
         a.field = "value";
         a.method();
     }
