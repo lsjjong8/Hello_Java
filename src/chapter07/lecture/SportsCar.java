3087cd411bfd9e99806485463b65ad4ebcea5e7a
Seung-jong <lsjjong8@naver.com>
2024-02-29 11:49:29 +0900

    1.4 final과 재정의 (상속)

--- src/chapter07/lecture/SportsCar.java
+++ src/chapter07/lecture/SportsCar.java
@@ -12,4 +12,13 @@ public class SportsCar extends Car {
         super(isCoupe ? 2 : 4);
         this.isCoupe = isCoupe;
     }
+
+    @Override
+    public void speedUp() { speed += 10; }
+
+    @Override
+    public final void stop() {
+        System.out.println("스포츠카를 멈춤");
+        speed = 0;
+    }
 }
