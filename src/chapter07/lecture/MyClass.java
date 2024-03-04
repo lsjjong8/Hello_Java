06c6301467ad8eb295847f76e5782e64328a520e
Seung-jong <lsjjong8@naver.com>
2024-03-04 17:00:22 +0900

    12. 인터페이스 활용 (인터페이스)

---
+++ src/chapter07/lecture/MyClass.java
@@ -0,0 +1,29 @@
+package chapter07.lecture;
+
+public class MyClass {
+    // 1) 필드
+    RemoteControl rc = new Television();
+
+    // 2) 생성자
+    public MyClass() {
+    }
+
+    MyClass(RemoteControl rc) {
+        this.rc = rc;
+        rc.turnOn();
+        rc.setVolume(5);
+    }
+
+    // 3) 메소드 내 로컬변수
+    void methodA() {
+        RemoteControl rc = new Audio();
+        rc.turnOn();
+        rc.setVolume(5);
+    }
+
+    // 메소드 내 매개변수
+    void methodB(RemoteControl rc) {
+        rc.turnOn();
+        rc.setVolume(5);
+    }
+}
