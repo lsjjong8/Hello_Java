803060b3b9e7fe8d0e2a0242caab098ea28a6603
Seung-jong <lsjjong8@naver.com>
2024-03-06 21:51:01 +0900

    7. 숫자 거꾸로 읽기 (메서드)

---
+++ src/chapter06/InverseNumber.java
@@ -0,0 +1,49 @@
+package chapter06;
+
+import java.util.Scanner;
+
+public class InverseNumber {
+    static Scanner sc = new Scanner(System.in);
+
+    static int readPlusInt() {
+        int input;
+        do {
+            System.out.print("양의 정수값:");
+            input = sc.nextInt();
+        } while(input <= 0);
+        return input;
+    }
+
+    static int retry() {
+        int input;
+        do {
+            System.out.print("다시 한 번? <Yes...1/No...0>:");
+            input = sc.nextInt();
+        } while(input!=1 && input!=0);
+        return input;
+    }
+
+    public static String printInverseNumber(int n) {
+        StringBuilder result = new StringBuilder();
+
+        while(n > 0) {
+            String intString = Integer.toString(n%10);
+            result.append(intString);
+            n /= 10;
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        int x;
+        do {
+            int n = readPlusInt();
+
+            System.out.print("반대로 읽으면 " + printInverseNumber(n) + "입니다.");
+
+            x = retry();
+
+        } while(x == 1);
+    }
+}
