0a4201a957f17bd60bdf4fe6c2fa58d14426a877
Seung-jong <lsjjong8@naver.com>
2024-03-06 20:45:08 +0900

    2. 최소값 메서드 (메서드)

---
+++ src/chapter06/Min3.java
@@ -0,0 +1,26 @@
+package chapter06;
+
+import java.util.Scanner;
+
+public class Min3 {
+    // a, b, c의 최솟값 반환
+    static int min(int a, int b, int c) {
+        int min = a;
+        if(b < min) min = b;
+        if(c < min) min = c;
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("정수 a:");
+        int a = sc.nextInt();
+        System.out.print("정수 b:");
+        int b = sc.nextInt();
+        System.out.print("정수 c:");
+        int c = sc.nextInt();
+
+        System.out.println("최소값은 " + min(a, b, c) + "입니다.");
+    }
+}
