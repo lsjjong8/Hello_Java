25ce9699c50e1b77bc669bb35da2cede393ab3c7
Seung-jong <lsjjong8@naver.com>
2024-03-11 14:48:45 +0900

    13.3 좌표 테스트3 (참조변수 초기값)

---
+++ src/chapter06/CoordinateTester3Error.java
@@ -0,0 +1,22 @@
+package chapter06;
+
+import java.util.Scanner;
+
+public class CoordinateTester3Error {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("좌표는 몇 개:");
+        int num = sc.nextInt();
+
+        Coordinate[] a = new Coordinate[num];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i].set(5.5, 7.7);
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("a[%d] = (%.1f, %.1f)\n", i, a[i].getX(), a[i].getY());
+        }
+    }
+}
