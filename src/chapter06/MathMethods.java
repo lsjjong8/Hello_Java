b68e68b9fea776b91c674b6783f50c00748fdea1
Seung-jong <lsjjong8@naver.com>
2024-03-12 14:09:50 +0900

    20.1 Math 클래스 (정적 멤버 임포트)

--- src/chapter06/MathMethods.java
+++ src/chapter06/MathMethods.java
@@ -1,10 +1,13 @@
 package chapter06;
 
 import java.util.Scanner;
+import static java.lang.Math.*;
+import static java.lang.System.in;
+import static java.lang.System.out;
 
 public class MathMethods {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(in);
 
         System.out.print("실수값:");
         double x = sc.nextDouble();
@@ -14,8 +17,8 @@ public class MathMethods {
         // Math class : 수치 계산 관련 메서드를 모은 것, 비슷한 부품을 모아둠
         // 비슷한 기능의 메서드나 상수를 모아서 캡슐화 (유틸리티 클래스)
         // <-> 데이터와 데이터 처리를 위한 로직을 캡슐화(객체)
-        System.out.println("절대값:" + Math.abs(x));
-        System.out.println("제곱근:" + Math.sqrt(x));
-        System.out.println("면적:" + Math.PI * x * x);
+        out.println("절대값:" + abs(x));
+        out.println("제곱근:" + sqrt(x));
+        out.println("면적:" + PI * x * x);
     }
 }
