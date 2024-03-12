a4b750abd2be014961e15e3b471ebaee5b4f212c
Seung-jong <lsjjong8@naver.com>
2024-03-12 12:17:28 +0900

    19. 정수형의 표현 범위 (Wrapper 클래스)

---
+++ src/chapter06/IntegerLimits.java
@@ -0,0 +1,11 @@
+package chapter06;
+
+public class IntegerLimits {
+    public static void main(String[] args) {
+        System.out.println("정수형의 표현 범위");
+        System.out.println("byte형:" + Byte.MIN_VALUE + " ~ " + Byte.MAX_VALUE);
+        System.out.println("short형:" + Short.MIN_VALUE + " ~ " + Short.MAX_VALUE);
+        System.out.println("int형:" + Integer.MIN_VALUE + " ~ " + Integer.MAX_VALUE);
+        System.out.println("long형:" + Long.MIN_VALUE + " ~ " + Long.MAX_VALUE);
+    }
+}
