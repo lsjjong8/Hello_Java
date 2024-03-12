052da9f6b1c7908ad1f99f56d78361d59d13fcc9
Seung-jong <lsjjong8@naver.com>
2024-03-12 13:59:23 +0900

    15.3 계좌 클래스 (인스턴스 초기화 블록)

--- src/chapter06/Account.java
+++ src/chapter06/Account.java
@@ -1,19 +1,30 @@
 package chapter06;
 
 public class Account {
+    private static int counter = 0; // 몇 번까지 식별 번호를 부여했는가
     private String name;    // 계좌 명의
     private String no;      // 계좌 번호
     private long balance;   // 예금 잔고
     private Day openDay;    // 계좌 개설일
+    private int id; // 식별 번호
 
+    // 인스턴스 초기화 블록
+    {
+        id = ++counter; // 식별 번호
+        System.out.println("은행 계좌를 개설해주셔서 감사합니다.");
+    }
+    
+    // 생성자
+    public Account(String name, String no) { // 생성자 (예금액은 0이고 계좌 개설일은 오늘)
+        this(name, no, 0, new Day());
+    }
 
     public Account(String name, String no, long balance, Day openDay) {
         this.name = name;
         this.no = no;
         this.balance = balance;
-        this.openDay = openDay;
+        this.openDay = new Day(openDay);
     }
-
 
     public String getName() {
         return name;
@@ -28,9 +39,12 @@ public class Account {
     }
 
     public Day getOpenDay() {
-        return openDay;
+        return new Day(openDay);
     }
 
+    public int getId() {
+        return id;
+    }
 
     // k원 입금
     void deposit(long k) {
