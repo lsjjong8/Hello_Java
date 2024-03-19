e60448f7cc5fbecd2dacf7f6751e7ad0ea18c703
Seung-jong <lsjjong8@naver.com>
2024-03-19 15:06:04 +0900

    7. 리소스 절대 경로 얻기 (Class)

---
+++ src/chapter11/lecture/ResourcePathExample.java
@@ -0,0 +1,13 @@
+package chapter11.lecture;
+
+public class ResourcePathExample {
+    public static void main(String[] args) {
+        Class clazz = ClassExample.class;
+
+        String photo1Path = clazz.getResource("photo1.png").getPath();
+        String photo2Path = clazz.getResource("images/photo2.jpg").getPath();
+
+        System.out.println(photo1Path);
+        System.out.println(photo2Path);
+    }
+}
