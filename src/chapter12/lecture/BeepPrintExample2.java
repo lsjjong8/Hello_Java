4280cefa7a92aaef5251dc01c6aa18f10606125d
Seung-jong <lsjjong8@naver.com>
2024-03-21 12:35:16 +0900

    1.3 익명 구현 객체 (Thread)

--- src/chapter12/lecture/BeepPrintExample2.java
+++ src/chapter12/lecture/BeepPrintExample2.java
@@ -1,5 +1,7 @@
 package chapter12.lecture;
 
+import java.awt.*;
+
 public class BeepPrintExample2 {
     public static void main(String[] args) {
         System.out.println("메인 메소드 시작");
@@ -14,6 +16,23 @@ public class BeepPrintExample2 {
                 Thread.sleep(500);
             } catch (Exception e) {}
         }
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("익명 작업 시작");
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("익명");
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                        e.getStackTrace();
+                    }
+                }
+                System.out.println("익명 작업 종료");
+            }
+        });
+        thread2.start();
 
         System.out.println("메인 메소드 종료");
     }
