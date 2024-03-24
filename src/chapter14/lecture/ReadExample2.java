ad6c3fea4df9cbbfac914aa378778694cd1a0abe
Seung-jong <lsjjong8@naver.com>
2024-03-24 14:36:53 +0900

    5. read(byte[] b) 메소드 (바이트 입력 스트림)

--- src/chapter14/lecture/ReadExample2.java
+++ src/chapter14/lecture/ReadExample2.java
@@ -8,7 +8,7 @@ public class ReadExample2 {
     public static void main(String[] args) throws IOException {
         InputStream is = new FileInputStream("C:/Temp/test2.db");
 
-        byte[] buffer = new byte[100];
+        byte[] buffer = new byte[2];
 
         while(true) {
             int readByteNum = is.read(buffer); // 배열 길이만큼 읽기
@@ -18,6 +18,8 @@ public class ReadExample2 {
             for (int i = 0; i < readByteNum; i++) {
                 System.out.println(buffer[i]);
             }
+
+            System.out.println();
         }
 
         is.close();
