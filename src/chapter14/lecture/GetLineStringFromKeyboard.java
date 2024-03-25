d9d14cdc7db955ac2e8b144a32280a3dac9cc422
Seung-jong <lsjjong8@naver.com>
2024-03-25 11:22:07 +0900

    19. System.in (입출력 관련 API)

---
+++ src/chapter14/lecture/GetLineStringFromKeyboard.java
@@ -0,0 +1,25 @@
+package chapter14.lecture;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class GetLineStringFromKeyboard {
+    public static void main(String[] args) throws Exception {
+        // InputStream을 Reader로 변환하고 다시 BufferedReader를 연결
+        InputStream is = System.in;
+        Reader reader = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(reader);
+
+        while(true) {
+            System.out.print("입력하세요: ");
+            String lineStr = br.readLine(); // 라인 단위로 문자열을 읽음
+            if(lineStr.equals("q") || lineStr.equals("quit")) break; // q 또는 quit을 읽었을 때 while 반복문 종료
+            System.out.print("입력된 내용: "+ lineStr);
+            System.out.println();
+        }
+
+        br.close(); // 입력 스트림 닫기
+    }
+}
