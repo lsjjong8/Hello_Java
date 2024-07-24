b28efe98b16e9f47250e5e5c925fc5c9bdbe32f9
Lee Seungjong <lsjjong8@naver.com>
2024-07-24 13:47:16 +0900

    35. 왼쪽 위가 직각인 이등변 삼각형 (for문, 수정)

--- src/chapter04/IsoscelesTriangleLU.java
+++ src/chapter04/IsoscelesTriangleLU.java
@@ -10,8 +10,8 @@ public class IsoscelesTriangleLU {
         System.out.print("단수는? :");
         int n = sc.nextInt();
 
-        for(int i=1; i<=n; i++) {
-            for (int j=1; j<=n-i+1; j++) {
+        for(int i=0; i<n; i++) {
+            for (int j=0; j<n-i; j++) {
                 System.out.print("*");
             }
             System.out.println();
