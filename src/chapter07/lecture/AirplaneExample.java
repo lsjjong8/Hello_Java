e9461abe3cfbf101c52f5acbc2b151fef0cf7eb0
Lee Seungjong <lsjjong8@naver.com>
2024-08-08 16:25:12 +0900

    3. super.fly() (오버라이딩)

--- src/chapter07/lecture/AirplaneExample.java
+++ src/chapter07/lecture/AirplaneExample.java
@@ -1,4 +1,14 @@
 package chapter07.lecture;
 
 public class AirplaneExample {
+    public static void main(String[] args) {
+        SupersonicAirplane sa = new SupersonicAirplane();
+        sa.takeOff();
+        sa.fly();
+        sa.flyMode = SupersonicAirplane.SUPERSONIC;
+        sa.fly();
+        sa.flyMode = SupersonicAirplane.NORMAL;
+        sa.fly();
+        sa.land();
+    }
 }
--- src/chapter07/lecture/SupersonicAirplane.java
+++ src/chapter07/lecture/SupersonicAirplane.java
@@ -1,4 +1,18 @@
 package chapter07.lecture;
 
-public class SupersonicAirplane {
+public class SupersonicAirplane extends AirPlane {
+
+    public static final int NORMAL = 1;
+    public static final int SUPERSONIC = 2;
+
+    public int flyMode = NORMAL;
+
+    @Override
+    public void fly() {
+        if(flyMode == SUPERSONIC) {
+            System.out.println("초음속비행합니다.");
+        } else {
+            super.fly(); // Airplane 객체의 fly()메소드 호출
+        }
+    }
 }
