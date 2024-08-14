d28d5ca7215b88722297aff6df8f3daf57ae67c7
Lee Seungjong <lsjjong8@naver.com>
2024-08-14 10:37:46 +0900

    1. A 클래스 내 중첩 (중첩 클래스, Java 11)

--- src/chapter08/lecture/A.java
+++ src/chapter08/lecture/A.java
@@ -1,38 +1,54 @@
 package chapter08.lecture;
 
+// Java version 11 기준
 public class A {
     A() {
         System.out.println("A 객체가 생성됨");
     }
 
+    // 인스턴스 멤버 클래스 (non-static, Heap 영역에 종속)
     class B {
         B() {
             System.out.println("B 객체가 생성됨");
         }
+
+        // 인스턴스 멤버
         int field1;
-        // static int field2;
         void method1() {}
+
+        // 정적 멤버
+        // static int field2;
         // static void method2() {}
     }
 
+    // 정적 멤버 클래스 (static, Method 영역에 종속)
     static class C {
         C() {
             System.out.println("C 객체가 생성됨");
         }
+
+        // 인스턴스 멤버
         int field1;
-        static int field2;
         void method1() {}
+
+        // 정적 멤버
+        static int field2;
         static void method2() {}
     }
 
+    // 로컬 클래스(메소드 내부, Stack 영역에 종속)
     void method() {
         class D {
             D() {
                 System.out.println("D 객체가 생성됨");
             }
+
+            // 인스턴스 멤버
             int field1;
-            // static int field2;
             void method1() {}
+
+            // 정적 멤버
+            // static int field2;
             // static void method2() {}
         }
 
