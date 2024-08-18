4e6cc1e3102fd078b630c526a28c6a00269fc196
Lee Seungjong <lsjjong8@naver.com>
2024-08-19 08:00:57 +0900

    1. 지네릭 클래스의 객체 생성과 사용

---
+++ src/chapter09/FruitBoxEx1.java
@@ -0,0 +1,70 @@
+package chapter09;
+
+import java.util.ArrayList;
+
+public class FruitBoxEx1 {
+    public static void main(String[] args) {
+        Box<Fruit> fruitBox = new Box<>();
+        Box<Apple> appleBox = new Box<>();
+        Box<Toy> toyBox = new Box<>();
+        // Box<Grape> appleBox = new Box<Apple>(); // 에러. 타입 불일치
+
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Apple()); // OK. void add(Fruit item)
+
+        appleBox.add(new Apple());
+        appleBox.add(new Apple());
+        // appleBox.add(new Toy()); // 에러. Box<Apple>에는 Apple만 담을 수 있음
+
+        toyBox.add(new Toy());
+        // toyBox.add(new Apple()); // 에러. Box<Toy>에는 Apple을 담을 수 있음
+
+        System.out.println(fruitBox);
+        System.out.println(appleBox);
+        System.out.println(toyBox);
+        // main의 끝
+    }
+}
+
+class Box<T> {
+    ArrayList<T> list = new ArrayList<>();
+    void add(T item) {
+        list.add(item);
+    }
+
+    T get(int i) {
+        return list.get(i);
+    }
+
+    int size() { return list.size(); }
+
+    public String toString() { return list.toString(); }
+}
+
+class Fruit {
+    @Override
+    public String toString() {
+        return "Fruit";
+    }
+}
+
+class Apple extends Fruit {
+    @Override
+    public String toString() {
+        return "Apple";
+    }
+}
+
+class Grape extends Fruit {
+    @Override
+    public String toString() {
+        return "Grape";
+    }
+}
+
+class Toy {
+    @Override
+    public String toString() {
+        return "Toy";
+    }
+}
