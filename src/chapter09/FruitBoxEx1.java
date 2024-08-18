11f85e15367ebcbde8412934d25136c64696b1ed
Lee Seungjong <lsjjong8@naver.com>
2024-08-19 08:05:19 +0900

    2. 제한된 지네릭 클래스

--- src/chapter09/FruitBoxEx1.java
+++ src/chapter09/FruitBoxEx1.java
@@ -4,44 +4,43 @@ import java.util.ArrayList;
 
 public class FruitBoxEx1 {
     public static void main(String[] args) {
-        Box<Fruit> fruitBox = new Box<>();
-        Box<Apple> appleBox = new Box<>();
-        Box<Toy> toyBox = new Box<>();
-        // Box<Grape> appleBox = new Box<Apple>(); // 에러. 타입 불일치
+        FruitBox<Fruit> fruitBox = new FruitBox<Fruit>();
+        FruitBox<Apple> appleBox = new FruitBox<Apple>();
+        FruitBox<Grape> grapeBox = new FruitBox<Grape>();
+        // FruitBox<Grape> grapeBox = new FruitBox<Apple>();  // 에러. 타입 불일치
+        // FruitBox<Toy> toyBox = new FruitBox<Toy>();      // 에러.
 
         fruitBox.add(new Fruit());
-        fruitBox.add(new Apple()); // OK. void add(Fruit item)
-
+        fruitBox.add(new Apple());
+        fruitBox.add(new Grape());
         appleBox.add(new Apple());
-        appleBox.add(new Apple());
-        // appleBox.add(new Toy()); // 에러. Box<Apple>에는 Apple만 담을 수 있음
+        // appleBox.add(new Grape()); // 에러. Grape는 Apple의 자손이 아님
+        grapeBox.add(new Grape());
 
-        toyBox.add(new Toy());
-        // toyBox.add(new Apple()); // 에러. Box<Toy>에는 Apple을 담을 수 있음
-
-        System.out.println(fruitBox);
-        System.out.println(appleBox);
-        System.out.println(toyBox);
-        // main의 끝
+        System.out.println("fruitBox-" + fruitBox);
+        System.out.println("appleBox-" + appleBox);
+        System.out.println("grapeBox-" + grapeBox);
     }
 }
 
 class Box<T> {
-    ArrayList<T> list = new ArrayList<>();
+    ArrayList<T> list = new ArrayList<T>();
     void add(T item) {
         list.add(item);
     }
-
     T get(int i) {
         return list.get(i);
     }
-
+    ArrayList<T> getList() { return list; }
     int size() { return list.size(); }
-
     public String toString() { return list.toString(); }
 }
 
-class Fruit {
+class FruitBox<T extends Fruit & Eatable> extends Box<T> {}
+
+interface Eatable {}
+
+class Fruit implements Eatable {
     @Override
     public String toString() {
         return "Fruit";
